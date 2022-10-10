988da6f79c4af61857d43d620f7d0677ff087f69
phithithuhuong <thu872315@gmail.com>
2022-10-10 14:09:23 +0700

    SuDungToanTu

---
+++ index.js
@@ -0,0 +1,8 @@
+ inputWidth = prompt("Enter the width");
+ inputHeight = prompt(" Enter the height");
+ let width = parseInt("10");
+ let height = parseInt("20");
+ let area = width * height;
+ document.write("The area is:" + area);
+
+
